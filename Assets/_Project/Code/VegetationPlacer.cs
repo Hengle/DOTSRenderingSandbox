22332e2fdd19b7cbaff7f803006a4d28a046847a
PhilSA <philippe.stamand1@gmail.com>
2019-05-19 13:34:18 -0400

    init

---
+++ Assets/_Project/Code/VegetationPlacer.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Mathematics;
+using Unity.Transforms;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(VegetationPlacer))]
+public class VegetationPlacerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        //if (GUILayout.Button("Place"))
+        //{
+        //    VegetationPlacer v = target as VegetationPlacer;
+        //    v.SpawnVegetation();
+        //}
+    }
+}
+#endif
+
+public enum VegetationSpawnerMethod
+{
+    GameObject,
+    MeshCombine,
+    DOTS,
+}
+
+public class VegetationPlacer : MonoBehaviour
+{
+    public VegetationSpawnerMethod Method;
+    public GameObject VegetationPrefab;
+    public int Quantity = 1000;
+    public BoxCollider Bounds;
+    public Collider OnCollider;
+
+    private EntityManager _entityManager;
+    private Entity _prefabEntity;
+
+    private Transform _spawnRoot;
+    private Transform _tmpTransform;
+    private Mesh _finalMesh;
+    private Mesh _grassMesh;
+    private Material _grassMat;
+    private CombineInstance[] _combineInstances;
+
+    private void Start()
+    {
+        SpawnVegetation();
+    }
+
+    public void SpawnVegetation()
+    {
+        if (Method == VegetationSpawnerMethod.DOTS)
+        {
+            _prefabEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(VegetationPrefab, World.Active);
+            _entityManager = World.Active.EntityManager;
+        }
+        else if (Method == VegetationSpawnerMethod.MeshCombine)
+        {
+            _finalMesh = new Mesh();
+            _finalMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            _tmpTransform = new GameObject("TMP").transform;
+            _grassMesh = VegetationPrefab.GetComponentInChildren<MeshFilter>().sharedMesh;
+            _grassMat = VegetationPrefab.GetComponentInChildren<MeshRenderer>().sharedMaterial;
+            _combineInstances = new CombineInstance[Quantity];
+        }
+        else if (Method == VegetationSpawnerMethod.GameObject)
+        {
+            _spawnRoot = new GameObject(this.gameObject.name + "_Spawn").transform;
+        }
+
+        Transform thisTransform = this.transform;
+        float xsize = Bounds.size.x * 0.5f;
+        float ysize = Bounds.size.y * 0.5f;
+        float zsize = Bounds.size.z * 0.5f;
+        Vector3 highBoundsCenter = thisTransform.position + Bounds.center + (thisTransform.rotation * Vector3.up * ysize);
+
+        // Spawn
+        for (int i = 0; i < Quantity; i++)
+        {
+            Vector3 randOrigin = highBoundsCenter + (thisTransform.rotation * new Vector3(UnityEngine.Random.Range(-xsize, xsize), 0f, UnityEngine.Random.Range(-zsize, zsize)));
+
+            Ray r = new Ray(randOrigin , - thisTransform.up);
+            if(OnCollider.Raycast(r, out RaycastHit hit, ysize * 2f))
+            {
+                Vector3 upDir = hit.normal;
+                Quaternion randomRot = Quaternion.AngleAxis(UnityEngine.Random.Range(0f, 30f), Vector3.right);
+                randomRot = Quaternion.AngleAxis(UnityEngine.Random.Range(0f, 360f), Vector3.up) * randomRot;
+                randomRot = Quaternion.FromToRotation(Vector3.up, upDir) * randomRot;
+
+                SpawnOne(i, hit.point, randomRot);
+            }
+        }
+
+        // Mesh combine
+        if (Method == VegetationSpawnerMethod.MeshCombine)
+        {
+            _finalMesh.CombineMeshes(_combineInstances);
+            _finalMesh.Optimize();
+            _finalMesh.OptimizeIndexBuffers();
+            _finalMesh.OptimizeReorderVertexBuffer();
+            GameObject grassesObject = new GameObject(this.gameObject.name + "_Spawn");
+            MeshFilter mf = grassesObject.AddComponent<MeshFilter>();
+            MeshRenderer mr = grassesObject.AddComponent<MeshRenderer>();
+            mf.sharedMesh = _finalMesh;
+            mr.sharedMaterial = _grassMat;
+
+            if (Application.isPlaying)
+            {
+                Destroy(_tmpTransform.gameObject);
+            }
+            else
+            {
+                DestroyImmediate(_tmpTransform.gameObject);
+            }
+        }
+    }
+
+    public void SpawnOne(int index, Vector3 pos, Quaternion rot)
+    {
+        if (Method == VegetationSpawnerMethod.DOTS)
+        {
+            var instance = _entityManager.Instantiate(_prefabEntity);
+            _entityManager.SetComponentData(instance, new Translation { Value = pos });
+            _entityManager.SetComponentData(instance, new Rotation { Value = rot });
+        }
+        else if (Method == VegetationSpawnerMethod.MeshCombine)
+        {
+            _tmpTransform.position = pos;
+            _tmpTransform.rotation = rot;
+            _combineInstances[index].subMeshIndex = 0;
+            _combineInstances[index].mesh = _grassMesh;
+            _combineInstances[index].transform = _tmpTransform.localToWorldMatrix;
+        }
+        else if (Method == VegetationSpawnerMethod.GameObject)
+        {
+            GameObject.Instantiate(VegetationPrefab, pos, rot, _spawnRoot);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# DOTS Rendering Sandbox
+
+![Alt Text](https://i.gyazo.com/3972f8dcfe4739a84db5cc252b70ab4b.gif)
+
+What is this project:
+This project contains a scene with a flat ground, and a spawner script that spawns any number of any given prefab on that ground using a selected method. The spawning methods right now are:
+- GameObject: creates each object with GameObject.Instantiate
+- DOTS: creates each object with EntityManager.Instantiate, after an ECS conversion of the original prefab
+- MeshCombine: gradually builds up a single mesh representing all of the spawned objects combined into one, and then creates a gameObject with that big mesh
+
+How to use:
+- Open in Unity 2019.1
+- Open the _Project/Scenes/Env scene
+- Find the "GrassSpawner" object in the scene
+- Set the Method, VegetationPrefab, and Quantity parameters
+- Press play and it'll spawn the grass
